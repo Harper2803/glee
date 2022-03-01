06b86f64a7b88ed31d1e23f2bdc00312124df7e4
Harper0803 <stepurkadenis0@gmail.com>
2022-03-01 14:38:40 +0600

    added product-list-2

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,28 @@
-$(function() {
+$(function(){
+  $('.product-recent__star').rateYo({
+    starWidth: '11px',
+    ratedFill: '#ffc000',
+    normalFill: '#d6d6d6',
+    spacing: "8px",
+    starSvg: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M12 17.25l-6.188 3.75 1.641-7.031-5.438-4.734 7.172-0.609 2.813-6.609 2.813 6.609 7.172 0.609-5.438 4.734 1.641 7.031z"></path></svg>'
+  });
+  $('.star').rateYo({
+    starWidth: '18px',
+    ratedFill: '#ffc000',
+    normalFill: '#d6d6d6',
+    spacing: "12px",
+    starSvg: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M12 17.25l-6.188 3.75 1.641-7.031-5.438-4.734 7.172-0.609 2.813-6.609 2.813 6.609 7.172 0.609-5.438 4.734 1.641 7.031z"></path></svg>'
+  });
+  $('.product-price__input').ionRangeSlider({
+    onStart: function (data){
+      $('.product-price__from').text(data.from),
+      $('.product-price__to').text(data.to)
+    },
+    onChange: function (data){
+      $('.product-price__from').text(data.from),
+      $('.product-price__to').text(data.to)
+    },
+  });
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false
@@ -13,4 +37,3 @@ $(function() {
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
 });
-  
